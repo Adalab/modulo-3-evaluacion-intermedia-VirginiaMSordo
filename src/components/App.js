9ec3fa0bf85da0405d63565cd9f6e520f33d5412
VirginiaMSordo <vickysalas23@gmail.com>
2022-11-28 10:58:33 +0100

    final version- remains localstorage

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,88 @@
-
+import contacts from '../data/contacts.json';
 import '../styles/App.scss';
+import { useState } from 'react';
 
 function App() {
+
+  // ESTADO
+
+  const [data, setData] = useState(contacts);
+  const [newAdalaber, setNewAdalaber] = useState({
+    "name": "",
+    "counselor": "",
+    "speciality": "",
+
+  });
+  const [search, setSearch] = useState('');
+
+
+
+  //constantes
+
+  const htmlData = data
+    .filter(
+      (contact) =>
+        contact.name.toLowerCase().includes(search.toLowerCase()) ||
+        contact.counselor.toLowerCase().includes(search.toLowerCase()) ||
+        contact.speciality.toLowerCase().includes(search.toLowerCase()))
+
+    .map((contact, index) => {
+      return (
+        <tr key={index}>
+          <td>{contact.name}</td>
+          <td>{contact.counselor}</td>
+          <td>{contact.speciality}</td>
+        </tr>
+      );
+
+    });
+
+  const handleSearch = (ev) => {
+    setSearch(ev.target.value);
+  };
+
+  // listeners
+
+  const handleInputs = (ev) => {
+    setNewAdalaber({ ...newAdalaber, [ev.target.name]: ev.target.value })
+
+  }
+
+  const handleAddNewAdalaber = (ev) => {
+    ev.preventDefault();
+    setData([...data, newAdalaber]);
+    setNewAdalaber({
+      "name": "",
+      "counselor": "",
+      "speciality": "",
+    });
+  }
+
+
+
+
+
+
+
   return (
     <div>
-      <table cellspacing='0'>
+      <header className="header">
+        <h1 className="header__title">Listado de Adalabers</h1>
+        <form>
+          <input
+            className="header__search"
+            autoComplete="off"
+            type="search"
+            name="search"
+            placeholder="Filtrar contactos por nombre"
+            onInput={handleSearch}
+            value={search}
+          />
+        </form>
+      </header>
 
+
+      <table cellSpacing='0'>
         {/* header */}
         <thead>
           <tr>
@@ -18,27 +95,11 @@ function App() {
 
         {/* body */}
         <tbody>
-
-          <tr>
-            <td>Create pretty table design</td>
-            <td>100%</td>
-            <td>Yes</td>
-          </tr>
-
-          <tr class="even">
-            <td>Take the dog for a walk</td>
-            <td>100%</td>
-            <td>Yes</td>
-          </tr>
-
-          <tr>
-            <td>Waste half the day on Twitter</td>
-            <td>20%</td>
-            <td>No</td>
-          </tr>
-
+          {htmlData}
         </tbody>
       </table>
+
+
       {/* new contact */}
       <form className="new-contact__form">
         <h2 className="new-contact__title">Añade un nuevo contacto</h2>
@@ -46,30 +107,35 @@ function App() {
           className="new-contact__input"
           type="text"
           name="name"
-          id="name"
           placeholder="Nombre"
+          onInput={handleInputs}
+          value={newAdalaber.name}
+
 
         />
         <input
           className="new-contact__input"
           type="text"
-          name="tutora"
-          id="tutora"
+          name="counselor"
           placeholder="Tutora"
+          onInput={handleInputs}
+          value={newAdalaber.counselor}
 
         />
         <input
           className="new-contact__input"
           type="text"
-          name="especialidad"
-          id="especialidad"
+          name="speciality"
           placeholder="Especialidad"
+          onInput={handleInputs}
+          value={newAdalaber.speciality}
 
         />
         <input
           className="new-contact__btn"
           type="submit"
           value="Añadir nueva Adalaber"
+          onClick={handleAddNewAdalaber}
 
         />
       </form>
